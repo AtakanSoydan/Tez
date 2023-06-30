563fc11b174bb3d9936e8fdc56f715eed09e5d0d
Barış Karakaya <baybarisk@outlook.com>
2023-06-30 23:46:10 +0300

    Quiz Scene Edited
    -> UI Added

--- Tez Projesi/Assets/Scripts/Quiz/AnswerScript.cs
+++ Tez Projesi/Assets/Scripts/Quiz/AnswerScript.cs
@@ -9,25 +9,29 @@ public class AnswerScript : MonoBehaviour
     public QuizManager quizManager;
 
     public Color StartColor;
+    public Sprite StartSprite;
+    public Sprite Red;
+    public Sprite Green;
 
     private void Start()
     {
-        StartColor = GetComponent<Image>().color;          
+        //StartColor = GetComponent<Image>().color;
+        StartSprite = GetComponent<Image>().sprite;
     }
 
     public void Answer()
     {
-        GetComponent<Image>().color = StartColor;
+        GetComponent<Image>().sprite = StartSprite;
 
         if (isCorrect)
         {
-            GetComponent<Image>().color = Color.green;
+            GetComponent<Image>().sprite = Green;
             Debug.Log("Correnct Answer");
             quizManager.Correct();
         }
         else
         {
-            GetComponent<Image>().color = Color.red;
+            GetComponent<Image>().sprite = Red;
             Debug.Log("Wrong Answer");
             quizManager.Wrong();
         }
