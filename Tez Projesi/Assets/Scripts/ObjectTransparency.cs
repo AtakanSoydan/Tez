548eafd2f135fd4e21fbf7c296fa52a7f5248a4c
Atakan Kutay Soydan <soydan810@gmail.com>
2023-07-06 20:59:00 +0300

    Scene Redesign

--- Tez Projesi/Assets/Scripts/ObjectTransparency.cs
+++ Tez Projesi/Assets/Scripts/ObjectTransparency.cs
@@ -2,35 +2,37 @@ using UnityEngine;
 
 public class ObjectTransparency : MonoBehaviour
 {
-    public Transform player;
-    public float fadeSpeed;
-    public float fadeAmount;
+    public KeyCode key = KeyCode.H;
+    public string newLayerName = "Wall";
+    public string newLayerName2 = "Default";
+
+    private bool isVisible = true;
+
+    public GameObject[] walls;
 
     private void Update()
     {
-        // Kameranýn konumu ve oyuncunun konumu arasýnda bir vektör oluþtur
-        Vector3 cameraToPlayer = player.position - Camera.main.transform.position;
-
-        // Kameranýn baktýðý yöne doðru bir ray oluþtur
-        Ray ray = new Ray(Camera.main.transform.position, cameraToPlayer);
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(ray, cameraToPlayer.magnitude);
-
-        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.red, 1f);
-
-        foreach (RaycastHit hit in hits)
+        if (Input.GetKeyDown(key))
         {
-            // Eðer raycast sonucunda bir obje bulunduysa ve obje bir Renderer bileþeni içeriyorsa
-            if (hit.collider != null && hit.collider.GetComponent<Renderer>() != null)
-            {
-                Renderer renderer = hit.collider.GetComponent<Renderer>();
-
-                Color materialColor = renderer.material.color;
-                float targetAlpha = Mathf.Lerp(materialColor.a, fadeAmount, fadeSpeed * Time.deltaTime);
-                materialColor.a = targetAlpha;
-                renderer.material.color = materialColor;
-
-            }
+            ChangeLayer();
+            isVisible = !isVisible;
         }
+    }
+
+    private void ChangeLayer()
+    {
+        foreach (var wall in walls)
+        {
+            if (isVisible)
+            {
+                wall.gameObject.layer = LayerMask.NameToLayer(newLayerName);
+            }
+            else
+            {
+                wall.gameObject.layer = LayerMask.NameToLayer(newLayerName2);
+            }
+
+        }
+        
     }
 }
