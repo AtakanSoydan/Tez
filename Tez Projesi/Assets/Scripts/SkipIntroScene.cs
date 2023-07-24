97014c594775eaccf7d005896694e1095b54d86d
Atakan Kutay Soydan <soydan810@gmail.com>
2023-07-24 13:53:10 +0300

    Intro Scene
    + intro sentences added
    + intro sentence animations
    + new script "IntroSkipScene"
    + timeline

---
+++ Tez Projesi/Assets/Scripts/SkipIntroScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipIntroScene : MonoBehaviour
+{
+    private void Update()
+    {
+        SkipIntro();
+    }
+    public void SkipIntro()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
